68ad1620e2c62d8851df687e53a6ce8d7c91ef26
bala-codeprism <bala.reddy@codeprism.in>
2023-11-08 17:24:01 +0530

    update service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,61 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Neo4jService } from 'nest-neo4j'
-
-
-@Controller()
+@Controller("movies")
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly neo4jService: Neo4jService) { }
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  async getMovies(): Promise<any> {
-    const res = await this.neo4jService.read(
-      `MATCH p=()-[r:KNOWS]->() RETURN p  `
-    )
-    // return res.records
-    const data: any = res.records
-    const nodes = {};
-    const edges = [];
-
-    for await (const record of data){
-      const startNode = record._fields[0].start;
-      const endNode = record._fields[0].end;
-
-      const startNodeId = startNode.identity.low;
-      const endNodeId = endNode.identity.low;
-
-      // Add nodes if they don't exist
-      if (!nodes[startNodeId]) {
-        nodes[startNodeId] = {
-          key: String(startNodeId),
-          attributes: startNode.properties // Corrected the property name to "attributes"
-        };
-      }
-
-      if (!nodes[endNodeId]) {
-        nodes[endNodeId] = {
-          key: String(endNodeId),
-          attributes: endNode.properties // Corrected the property name to "attributes"
-        };
-      }
-
-      // Extract edge information
-      const edge = {
-        key: record._fields[0].segments[0].relationship.identity.low,
-        source: String(startNodeId), // Ensure the source and target keys are strings
-        target: String(endNodeId),
-        type: record._fields[0].segments[0].relationship.type,
-        attributes:record._fields[0].segments[0].relationship.properties,
-        undirected: true
-      };
-
-      edges.push(edge);
-    }
-
-    const graphData = {
-      nodes: Object.values(nodes),
-      edges,
-    }
-    return graphData
+  async getMovies(@Query('cipher') cipherQuery:any): Promise<any> {
+    return await this.appService.getMovies(cipherQuery)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { Neo4jService } from 'nest-neo4j/dist';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly neo4jService: Neo4jService) { }
+  async getMovies(cipherQuery: any): Promise<any> {
+    try {
+      const res = await this.neo4jService.read(cipherQuery)
+      return res.records
+    } catch (error) {
+      throw error
+    }
   }
 }
